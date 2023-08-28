508a72965ddde2dfa776c2d4cbb0162cff327b75
HasilbekR <hreymbaev@gmail.com>
2023-08-29 00:40:45 +0500

    cors bean added

--- src/main/java/com/example/userservice/config/SecurityConfig.java
+++ src/main/java/com/example/userservice/config/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(Customizer.withDefaults())
                 .csrf().disable()
                 .authorizeHttpRequests((authorize) -> {
                     authorize
