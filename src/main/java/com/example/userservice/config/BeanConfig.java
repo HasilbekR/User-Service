fe09cfc426aeef84702151b338088393aaa3b37c
HasilbekR <hreymbaev@gmail.com>
2023-08-28 21:06:22 +0500

    cors bean added

--- src/main/java/com/example/userservice/config/BeanConfig.java
+++ src/main/java/com/example/userservice/config/BeanConfig.java
@@ -11,13 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 @Configuration
 public class BeanConfig {
