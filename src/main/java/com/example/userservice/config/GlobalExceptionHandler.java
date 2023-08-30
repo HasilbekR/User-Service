053af256c5f207f647a9821dc5d757d83086d185
HasilbekR <hreymbaev@gmail.com>
2023-08-30 10:29:08 +0500

    global exception updated

--- src/main/java/com/example/userservice/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/userservice/config/GlobalExceptionHandler.java
@@ -16,38 +16,38 @@ public class GlobalExceptionHandler {
             requestValidationExceptionHandler(
             RequestValidationException e
     ){
-        return ResponseEntity.ok().body(StandardResponse.<String>builder().status("400").message(e.getMessage()).build());
+        return ResponseEntity.status(400).body(StandardResponse.<String>builder().status("400").message(e.getMessage()).build());
     }
 
     @ExceptionHandler(value = {AuthenticationFailedException.class})
     public ResponseEntity<StandardResponse<String>> authenticationFailedExceptionHandler(
             AuthenticationFailedException e
     ){
-        return ResponseEntity.ok().body(StandardResponse.<String>builder().status("401").message(e.getMessage()).build());
+        return ResponseEntity.status(401).body(StandardResponse.<String>builder().status("401").message(e.getMessage()).build());
 
     }
     @ExceptionHandler(value = {AccessDeniedException.class})
-    public StandardResponse<String> AccessDeniedExceptionHandler(
+    public ResponseEntity<StandardResponse<String>> AccessDeniedExceptionHandler(
             AccessDeniedException e
     ){
-        return StandardResponse.<String>builder().status("403").message(e.getMessage()).build();
+        return ResponseEntity.status(403).body(StandardResponse.<String>builder().status("403").message(e.getMessage()).build());
     }
 
     @ExceptionHandler(value = {DataNotFoundException.class})
-    public StandardResponse<String> dataNotFoundExceptionHandler(
+    public ResponseEntity<StandardResponse<String>> dataNotFoundExceptionHandler(
             DataNotFoundException e){
-        return StandardResponse.<String>builder().status("404").message(e.getMessage()).build();
+        return ResponseEntity.status(404).body(StandardResponse.<String>builder().status("404").message(e.getMessage()).build());
 
     }
 
     @ExceptionHandler(value = {UniqueObjectException.class})
-    public StandardResponse<String> uniqueObjectExceptionHandler(
+    public ResponseEntity<StandardResponse<String>> uniqueObjectExceptionHandler(
             UniqueObjectException e){
-        return StandardResponse.<String>builder().status("401").message(e.getMessage()).build();
+        return ResponseEntity.status(401).body(StandardResponse.<String>builder().status("401").message(e.getMessage()).build());
     }
     @ExceptionHandler(value = {UserBadRequestException.class})
-    public StandardResponse<String> userBadRequestExceptionHandler(
+    public ResponseEntity<StandardResponse<String>> userBadRequestExceptionHandler(
             UserBadRequestException e){
-        return StandardResponse.<String>builder().status("400").message(e.getMessage()).build();
+        return ResponseEntity.status(400).body(StandardResponse.<String>builder().status("400").message(e.getMessage()).build());
     }
 }
