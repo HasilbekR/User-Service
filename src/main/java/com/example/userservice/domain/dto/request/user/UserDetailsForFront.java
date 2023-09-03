d76b1b6850eda34aa0a5ca4c881383f64592340b
HasilbekR <hreymbaev@gmail.com>
2023-09-03 15:05:50 +0500

    user details for front updated

--- src/main/java/com/example/userservice/domain/dto/request/user/UserDetailsForFront.java
+++ src/main/java/com/example/userservice/domain/dto/request/user/UserDetailsForFront.java
@@ -1,11 +1,13 @@
 package com.example.userservice.domain.dto.request.user;
 
+import com.example.userservice.domain.entity.user.Gender;
 import com.example.userservice.domain.entity.user.UserState;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,6 +19,9 @@ public class UserDetailsForFront {
     private UUID id;
     private String fullName;
     private UserState userState;
+    private String phoneNumber;
+    private LocalDate dateOfBirth;
+    private Gender gender;
     private List<String> roles;
     private List<String> permissions;
 
